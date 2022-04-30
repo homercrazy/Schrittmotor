8f80d0bd0ee40a59f911128cee77d659f1a0115b
homercrazy <homercrazy@gmx.de>
2022-04-30 13:59:53 +0200

    Justage hinzugefügt

--- Schrittmotor.ino
+++ Schrittmotor.ino
@@ -1,6 +1,11 @@
 
-#define Step 13
-#define Dir  15
+#define Step 13 // D7
+#define Dir  15 // D8
+
+#define justFor 4 // D2  # Pin für Vorwärtsjustage
+#define justBack 5 // D1  # Pin für Rückwärtsjustage
+#define closeDoor 2 // D4 
+
 
 #define MS1 1
 #define MS2 2
@@ -17,6 +22,9 @@ void setup() {
   pinMode(MS2,OUTPUT);
   pinMode(MS3,OUTPUT);
 
+  pinMode(justFor,INPUT_PULLUP);
+  pinMode(justBack,INPUT_PULLUP);
+  pinMode(closeDoor,INPUT_PULLUP);
 
 
 }
@@ -24,15 +32,50 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
-  
+ int delayMillisec=1; // Winkelgeschwindigkeit 1 ms entspricht 17,6°/s bei einem Übersetzungsverhältnis von 51:1
+ 
+ int steps=2550;  // Abgefahrener Winkelbereich 2550 Schritte entspricht 90° (200*51=10200 Schritte pro Umdrehung)
 
- int delayMillisec=1; 
- int steps=2550;
 
-  for(int a1=1;a1<steps;a1++){
-    
+
+////////////////////////////////////////////////////////////////////////
+// Justage der Hebelausrichtung 
+////////////////////////////////////////////////////////////////////////
+
+while (digitalRead(justFor)==LOW){
+
     digitalWrite(Dir,LOW);
+
+    digitalWrite(Step,HIGH);
+    delay(delayMillisec*10);
+    digitalWrite(Step,LOW);
+    delay(delayMillisec*10);
   
+}
+
+while (digitalRead(justBack)==LOW){
+
+    digitalWrite(Dir,HIGH);
+
+    digitalWrite(Step,HIGH);
+    delay(delayMillisec*10);
+    digitalWrite(Step,LOW);
+    delay(delayMillisec*10);
+  
+}
+
+////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+if (digitalRead(closeDoor)==LOW){
+
+
+  digitalWrite(Dir,LOW);
+  for(int a1=1;a1<steps;a1++){
+   
     //digitalWrite(MS1,HIGH);
     //digitalWrite(MS2,HIGH);
     //digitalWrite(MS3,HIGH);
@@ -46,10 +89,10 @@ void loop() {
 
   delay(5000);
 
+  digitalWrite(Dir,HIGH);
+
   for(int a1=1;a1<steps;a1++){
     
-    digitalWrite(Dir,HIGH);
-  
     //digitalWrite(MS1,HIGH);
     //digitalWrite(MS2,HIGH);
     //digitalWrite(MS3,HIGH);
@@ -61,9 +104,8 @@ void loop() {
 
   }
 
-  delay(1000);
-
-
+  
+}
   
 
 }
